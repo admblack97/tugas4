c0de909d5fed58729f1a252fd360101149041b2d
admblack97 <admblack97@gmail.com>
2018-01-02 22:37:44 +0800

    meolah variabel array 1 dimensi

--- array_1_dimensi.cpp
+++ array_1_dimensi.cpp
@@ -3,4 +3,71 @@
 using namespace std;
 
 int main(){
+	cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
+	//meolah variabel
+	double a1c[5]={4,4,5,3,3};
+	double a2c[5]={3,3,4,2,3};
+	double a3c[5]={5,4,2,2,2};
+	
+	double mc[5]={
+		max(a1c[0],max(a2c[0],a3c[0])),
+		max(a1c[1],max(a2c[1],a3c[1])),
+		max(a1c[2],max(a2c[2],a3c[2])),
+		max(a1c[3],max(a2c[3],a3c[3])),
+		max(a1c[4],max(a2c[4],a3c[4]))
+	};
+	
+	double Ra1c[5]={ 
+		(a1c[0]/mc[0]), 
+		(a1c[1]/mc[1]),
+		(a1c[2]/mc[2]),
+		(a1c[3]/mc[3]),
+		(a1c[4]/mc[4])
+	};
+	
+	double Ra2c[5]={ 
+		(a2c[0]/mc[0]), 
+		(a2c[1]/mc[1]),
+		(a2c[2]/mc[2]),
+		(a2c[3]/mc[3]),
+		(a2c[4]/mc[4])
+	};
+	
+	double Ra3c[5]={ 
+		(a3c[0]/mc[0]), 
+		(a3c[1]/mc[1]),
+		(a3c[2]/mc[2]),
+		(a3c[3]/mc[3]),
+		(a3c[4]/mc[4])
+	};
+		
+	double w[5]={5,3,4,4,3};
+	
+	double RWa1c[5]={
+		Ra1c[0]*w[0],
+		Ra1c[1]*w[1],
+		Ra1c[2]*w[2],
+		Ra1c[3]*w[3],
+		Ra1c[4]*w[4]
+	};
+	
+	double RWa2c[5]={
+		Ra2c[0]*w[0],
+		Ra2c[1]*w[1],
+		Ra2c[2]*w[2],
+		Ra2c[3]*w[3],
+		Ra2c[4]*w[4]
+	};
+	
+	double RWa3c[5]={
+		Ra3c[0]*w[0],
+		Ra3c[1]*w[1],
+		Ra3c[2]*w[2],
+		Ra3c[3]*w[3],
+		Ra3c[4]*w[4]
+	};
+	double ha[3];
+	
+	//proses penjumlahan dari R*W
+		
 }
