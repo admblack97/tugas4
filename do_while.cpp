536f5a15ddb545f0234110eaa203c1316047b86f
admblack97 <admblack97@gmail.com>
2018-01-03 18:06:45 +0800

    bobot W

--- do_while.cpp
+++ do_while.cpp
@@ -51,5 +51,9 @@ int main() {
 		apam3++;
 	}while(apam3<5);cout<<endl;
 	
+	double w1=5, w2=3, w3=4, w4=4, w5=2;
+	cout << endl << "BOBOT W :"<<endl<< w1<<" | "<< w2<<" | "<< w3<<" | "<< w4<<" | "<< w5<<" | "<<endl;
+	
+	
 	
 }
